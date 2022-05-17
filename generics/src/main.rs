1b4ab768f9f953d105ff2efa6addb8f692231b3b
onegronm <omarnegron92@hotmail.com>
2022-05-17 15:36:16 -0400

    generics

---
+++ generics/src/main.rs
@@ -0,0 +1,80 @@
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
+
+}
---
+++ generics/target/debug/deps/generics-5be73e65452c53dd.d
@@ -0,0 +1,5 @@
+/Users/omar.montero/code/rust/the-rust-programming-language/hello_world/generics/target/debug/deps/generics-5be73e65452c53dd: src/main.rs
+
+/Users/omar.montero/code/rust/the-rust-programming-language/hello_world/generics/target/debug/deps/generics-5be73e65452c53dd.d: src/main.rs
+
+src/main.rs:
